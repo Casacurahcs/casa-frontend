386ccefd1ba29c08dd7bc95c8365fd29ab024ac5
Casacurahcs <casacurahcs@gmail.com>
2025-06-14 18:35:28 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        nhsblue: '#005EB8',
+        nhsgreen: '#007F3B',
+      },
+    },
+  },
+  plugins: [],
+}
