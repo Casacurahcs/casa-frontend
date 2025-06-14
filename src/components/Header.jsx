5bbbe6a831e61a2fcd8d828420ea34fd132932c5
Casacurahcs <casacurahcs@gmail.com>
2025-06-14 19:02:27 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="bg-nhsblue text-white p-4 flex items-center gap-4">
-      <img src="/casa cura-01.png" alt="Logo" className="h-10" />
+      <img src="/logo.png" alt="Logo" className="h-10" />
       <h1 className="text-xl font-semibold">Patient Log</h1>
     </header>
   )
